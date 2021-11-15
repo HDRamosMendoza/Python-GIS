f73ab61c67feee98054c705189a13ef260d3df65
Daniel Ramos Mendoza <heber.daniel.ramos.mendoza@gmail.com>
2021-11-15 18:44:39 -0500

    Mapa de calor

---
+++ MiniProyectos/MapaDensidad/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+	Name App 	: Mapa de Densidad / Concentración / Calor
+	Description	: 
+	Created 	: Heber DANIEL Ramos Mendoza (HDRamosMendoza)
+	Email		: heber.daniel.ramos.mendoza@gmail.com
+	Position 	: GIS Developer
+	Location 	: Perú/Lima/SanJuanDeLurigancho
+	Social media: HDRamosMendoza
+	Phone		: 999130638
+	Web			: https://hdramosmendoza.github.io/Perfil-Profesional/
+	Copyright	: Heber Daniel Ramos Mendoza
+	Licence		: Favor de mencionar al autor
+"""
